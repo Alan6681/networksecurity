4602ede652dd63a9349f11c01892deb14ff56056
Alan6681 <alanaboamaegbe@gmail.com>
2025-10-31 12:44:22 +0100

    Data ingestion component completed

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -4,6 +4,7 @@ from networksecurity.logging.logger import logging
 ## Configuration of the Data ingestion config
 
 from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifacts
 import os
 import sys
 import pymongo
@@ -44,10 +45,55 @@ class DataIngestion:
             df.replace({"na": np.nan}, inplace=True)
             return df
         except Exception as e:
-            raise NetworkSecurityException
+            raise NetworkSecurityException(e, sys)
+        
+    
+    def export_data_to_feature_store(self, dataframe:pd.DataFrame):
+        """
+        Export data to feature store
+
+        """
+        try:
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            dataframe.to_csv(feature_store_file_path, index=False, header=True)
+            return dataframe
+        
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def split_data_as_train_test_split(self, dataframe:pd.DataFrame):
+        try:
+            train_set, test_set = train_test_split(
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+            )
+            logging.info("Performed the train test split on the dataframe")
+            logging.info("Exited train test split method of Data Ingestion class")
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            logging.info("Exporting train and test file paths. ")
+            train_set.to_csv(
+                self.data_ingestion_config.training_file_path, index=False, header=True
+            )
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
+            logging.info("Exported train and test file paths")
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
         
     def initiate_data_ingestion(self):
         try:
             dataframe = self.export_collection_as_dataframe()
+            dataframe = self.export_data_to_feature_store(dataframe)
+            self.split_data_as_train_test_split(dataframe)
+
+            dataingestionartifact = DataIngestionArtifacts(trained_file_path=self.data_ingestion_config.training_file_path, test_file_path=self.data_ingestion_config.testing_file_path)
+            return dataingestionartifact
+        
         except Exception as e:
             raise NetworkSecurityException(e, sys)
+        
+   
